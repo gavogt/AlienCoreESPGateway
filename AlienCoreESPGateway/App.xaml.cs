fa7d5c842041a6144f74fbca2a07b55724e6b066
gavogt <gvogt@asu.edu>
2025-07-13 05:51:56 -0700

    Add project files.

---
+++ AlienCoreESPGateway/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AlienCoreESPGateway
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "AlienCoreESPGateway" };
+        }
+    }
+}
