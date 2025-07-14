1eb44457e7390f0c259f6ae029a8ce68171ca5b7
gavogt <gvogt@asu.edu>
2025-07-14 09:00:01 -0700

    feat(gateway/ui): wire up Home.razor, register HubConnection in MauiProgram, bind HTTP port 5000 in EdgeGateway Program.cs

--- AlienCoreESPGateway/MauiProgram.cs
+++ AlienCoreESPGateway/MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.SignalR.Client;
 namespace AlienCoreESPGateway
 {
     public static class MauiProgram
@@ -13,12 +13,16 @@ namespace AlienCoreESPGateway
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-
+            builder.Services.AddSingleton(sp => new HubConnectionBuilder()
+            .WithUrl("http://192.168.1.162:5000/telemetryHub")
+            .WithAutomaticReconnect()
+            .Build()
+            );
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- EdgeGateway/Program.cs
+++ EdgeGateway/Program.cs
@@ -1,16 +1,30 @@
 
 using EdgeGateway;
+using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
 
-builder.Services.AddSingleton<IMqttClient>();
 builder.Services.AddHostedService<TelemetryForwarder>();
-
+builder.Services.AddSingleton<IMqttClient>(sp =>
+{
+    var factory = new MqttFactory();
+    var mqttClient = factory.CreateMqttClient();
+    var options = new MqttClientOptionsBuilder()
+        .WithClientId("EdgeGateway")
+        .WithTcpServer("192.168.1.164", 1883)
+        .WithCleanSession()
+        .Build();
+    mqttClient.ConnectAsync(options, CancellationToken.None).Wait();
+    return mqttClient;
+});
 var app = builder.Build();
-
+app.Urls.Clear();
+app.Urls.Add("http://192.168.1.162:5000");
 app.MapHub<TelemetryHub>("/telemetryHub");
 
 app.Run();
