e5039063a4843109f738fbccbaa17d9bd0a6a7e4
gavogt <gvogt@asu.edu>
2025-07-19 01:01:48 -0700

    feat(core): add User entity

---
+++ AlienCoreESPGateway/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCoreESPGateway
+{
+    public class User
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public User(string Email, string Password, string FirstName, string LastName)
+        {
+            this.Email = Email;
+            this.Password = Password;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+}
