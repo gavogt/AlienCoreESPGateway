1074f1bfa8417e0f2cef29c5219d336d97aead9a
gavogt <gvogt@asu.edu>
2025-07-19 03:00:03 -0700

    feat(auth): add RegisterDbContext
    Introduces RegisterDbContext to manage user registration data and configures the Users DbSet.

---
+++ AlienCoreESPGateway/RegisterDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlienCoreESPGateway
+{
+    public class RegisterDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public RegisterDBContext(DbContextOptions<RegisterDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Email);
+            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.FirstName).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.LastName).IsRequired();
+        }
+    }
+}
