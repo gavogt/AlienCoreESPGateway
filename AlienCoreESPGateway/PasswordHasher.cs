1e62edd7b41dba13e6d0a6db1f4b9cccddf72c32
gavogt <gvogt@asu.edu>
2025-07-19 17:51:07 -0700

    feat(auth): add password hasher and hash password on registration
    - Introduce PasswordHasher (wrapping ASP.NET Identity’s PasswordHasher)
    - In Registration.razor, call `model.Password = PasswordHasher.Hash(model.Password)` before submitting the form

---
+++ AlienCoreESPGateway/PasswordHasher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+
+namespace AlienCoreESPGateway
+{
+    public class PasswordHasher
+    {
+        private readonly PasswordHasher<object> _hasher = new();
+
+        public string HashPassword(string plaintextPassword)
+        {
+            if (string.IsNullOrEmpty(plaintextPassword))
+            {
+                throw new ArgumentException("Password cannot be null or empty", nameof(plaintextPassword));
+            }
+            else
+            {
+
+                return _hasher.HashPassword(null!, plaintextPassword);
+            }
+
+            
+        }
+
+        public bool Verify(string hashedPassword, string plaintextPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(null, hashedPassword, plaintextPassword);
+            return result != PasswordVerificationResult.Failed;
+
+        }
+    }
+}
