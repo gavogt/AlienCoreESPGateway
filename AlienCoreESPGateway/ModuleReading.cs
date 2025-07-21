3420d82a5781eb4fa31ffb8eb3cfb123a32024a3
gavogt <gvogt@asu.edu>
2025-07-20 21:34:07 -0700

    feat(telemetry): add TelemetryMessage & ModuleReading models
    - Introduce `TelemetryMessage` with `TimestampUnix` property
    - Add `ModuleReading` class to represent individual module type/value pairs
    - Compute local `DateTime` from Unix timestamp in the model for display

---
+++ AlienCoreESPGateway/ModuleReading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace AlienCoreESPGateway
+{
+    internal class ModuleReading
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = default!;
+
+        [JsonPropertyName("value")]
+        public double Value { get; set; }
+    }
+}
---
+++ AlienCoreESPGateway/TelemetryMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace AlienCoreESPGateway
+{
+    internal class TelemetryMessage
+    {
+        [JsonPropertyName("scoutId")]
+        public string ScoutId { get; set; } = default!;
+        [JsonPropertyName("timestamp")]
+        public long TimestampUnix { get; set; }
+
+        [JsonIgnore]
+        public DateTime Timestamp => DateTimeOffset.FromUnixTimeMilliseconds(TimestampUnix).LocalDateTime;
+        
+        [JsonPropertyName("modules")]
+
+        public List<ModuleReading> Modules { get; set; } = new List<ModuleReading>();
+    }
+}
