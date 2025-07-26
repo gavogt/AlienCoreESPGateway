703898d9ac46ddd3aca2780b42b59457e97e7eff
Gabriel <gvogt@asu.edu>
2025-07-26 16:08:10 -0700

    Add XenoBridge Program.cs to configure Host and RabbitMQ ConnectionFactory

--- XenoBridge/Program.cs
+++ XenoBridge/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using XenoBridge;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostCtx, services) =>
+    {
+        // read RabbitMQ settings from environment 
+        var rmqHost = hostCtx.Configuration["RABBITMQ_HOST"] ?? "quantumqueue";
+        var rmqUser = hostCtx.Configuration["RABBITMQ_USER"] ?? "scout";
+        var rmqPass = hostCtx.Configuration["RABBITMQ_PASS"] ?? "passw0rd";
 
-var host = builder.Build();
-host.Run();
+        services.AddSingleton(sp => new ConnectionFactory
+        {
+            HostName = rmqHost,
+            UserName = rmqUser,
+            Password = rmqPass
+        });
+
+        services.AddHostedService<Worker>();
+    })
+    .Build()
+    .Run();
