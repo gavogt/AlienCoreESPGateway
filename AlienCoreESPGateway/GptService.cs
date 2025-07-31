a1a08b2e330f864604fcb3ae109c46b6e83311e2
Gabriel <gvogt@asu.edu>
2025-07-31 14:17:24 -0700

    refactor(telemetry): extract collections into TelemetryDataService
    - Move NeuroRows, PlasmaRows, BioRows and Messages handling into TelemetryDataService
    - Inject TelemetryDataService in Telemetry.razor instead of local ObservableCollections

---
+++ AlienCoreESPGateway/GptService.cs
@@ -0,0 +1,19 @@
+﻿using OpenAI.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCoreESPGateway
+{
+    public class GptService
+    {
+        private readonly ChatClient _client;
+        private readonly TelemetryModuleReading _telemetry;
+        public async Task AskGPT()
+        {
+            
+        }
+    }
+}
---
+++ AlienCoreESPGateway/TelemetryDataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCoreESPGateway
+{
+    internal class TelemetryDataService
+    {
+
+        // three series buffers, auto‐refreshing via ObservableCollection
+        public readonly ObservableCollection<ModuleRow> _neuroRows = new();
+        public readonly ObservableCollection<ModuleRow> _plasmaRows = new();
+        public readonly ObservableCollection<ModuleRow> _bioRows = new();
+
+        public const int MaxPoints = 20; // max points in each series
+
+        public List<TelemetryMessage> messages { get; } = new();
+
+        public void AddMessage(TelemetryMessage msg)
+        {
+            // table history
+            messages.Add(msg);
+            if (messages.Count > MaxPoints)
+                messages.RemoveAt(0);
+
+            // add to row based on type
+            foreach (var mod in msg.Modules)
+            {
+                var pt = new ModuleRow(msg.Timestamp, mod.Value);
+                switch (mod.Type.ToUpperInvariant())
+                {
+                    case "NEURO":
+                        _neuroRows.Add(pt);
+                        if (_neuroRows.Count > MaxPoints) _neuroRows.RemoveAt(0);
+                        break;
+                    case "PLASMA":
+                        _plasmaRows.Add(pt);
+                        if (_plasmaRows.Count > MaxPoints) _plasmaRows.RemoveAt(0);
+                        break;
+                    case "BIO":
+                        _bioRows.Add(pt);
+                        if (_bioRows.Count > MaxPoints) _bioRows.RemoveAt(0);
+                        break;
+                }
+            }
+        }
+    }
+}
